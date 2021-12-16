436ab121a21792f0d619cd8b7a7a66de8b9e1e4b
drobotk <pawwwll@gmail.com>
2021-12-16 21:52:51 +0100

    Add lxml requirement, fix proactor event loop bug

--- vulcan_scraper/main.py
+++ vulcan_scraper/main.py
@@ -1,5 +1,9 @@
 import logging
 import asyncio
+import sys
+if sys.version_info[0] == 3 and sys.version_info[1] >= 8 and sys.platform.startswith('win'):
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
 from typing import *
 
 from .error import *
