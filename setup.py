30407ef9f4ee253ca43dfb2ce4d06f6c67cc6933
drobotk <pawwwll@gmail.com>
2022-01-16 00:25:55 +0100

    Version 0.1.3

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
-VERSION = "0.1.3"
+VERSION = "0.1.4"
 
 setup(
     name="vulcan-sdk-py",
     version=VERSION,
     maintainer="drobotk",
     packages=find_packages(),
-    install_requires=open("requirements.txt").read().split("\n"),
+    install_requires=open("requirements.txt").readlines(),
 )
